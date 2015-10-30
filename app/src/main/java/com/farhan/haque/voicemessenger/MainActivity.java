bd0f7f3b1c675d8a08f40e7ea78ebca3b1d9c4fe
farhan071024 <mdfarhanhaque@gmail.com>
2015-10-30 16:04:46 -0500

    2nd update

--- app/src/main/java/com/farhan/haque/voicemessenger/MainActivity.java
+++ app/src/main/java/com/farhan/haque/voicemessenger/MainActivity.java
@@ -1,34 +1,82 @@
 package com.farhan.haque.voicemessenger;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.provider.ContactsContract;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-    Button button;
+    TextView titleTextView;
     private static final int SPEECH_REQUEST_CODE = 0;
+    public static final String ACTION_SMS_SENT = "com.farhan.haque.voicemessenger";
+    String phoneNumber,spokenText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button= (Button) findViewById(R.id.button);
+        titleTextView= (TextView) findViewById(R.id.titleTextView);
+        if(savedInstanceState!=null){
+            savedInstanceState.getString("spokenText","");
+        }
+        // Register broadcast receivers for SMS sent and delivered intents
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String message = null;
+                boolean error = true;
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        message = "Message sent!";
+                        error = false;
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        message = "Error.";
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        message = "Error: No service.";
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        message = "Error: Null PDU.";
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        message = "Error: Radio off.";
+                        break;
+                }
+                titleTextView.setText(message);
+                titleTextView.setTextColor(error ? Color.RED : Color.GREEN);
+
+            }
+        }, new IntentFilter(ACTION_SMS_SENT));
     }
+
+    // This callback is invoked when the Speech Recognizer returns.
+// This is where you process the intent and extract the speech text from the intent.
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent data) {
         if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
             List<String> results = data.getStringArrayListExtra(
                     RecognizerIntent.EXTRA_RESULTS);
-            String spokenText = results.get(0);
+            spokenText = results.get(0);
             // Do something with spokenText
             Toast.makeText(this,spokenText,Toast.LENGTH_LONG).show();
         }
@@ -42,10 +90,68 @@ public class MainActivity extends ActionBarActivity {
 // Start the activity, the intent will be populated with the speech text
         startActivityForResult(intent, SPEECH_REQUEST_CODE);
     }
+    //  Find contact based on name.
+ public void findNumbers(View v){
+     String NAME= "Farhan";
+     ContentResolver cr = getContentResolver();
+     Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null,
+             "DISPLAY_NAME = '" + NAME + "'", null, null);
+     if (cursor.moveToFirst()) {
+         String contactId =
+                 cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+         //
+         //  Get all phone numbers.
+         //
+         Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                 ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+         while (phones.moveToNext()) {
+             String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+             int type = phones.getInt(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+          /*   switch (type) {
+                 case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+                     // do something with the Home number here...
+                     break;
+                 case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+                     // do something with the Mobile number here...
+                     break;
+                 case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+                     // do something with the Work number here...
+                     break;
+             }*/
+             if(type==ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE) {
+                 Toast.makeText(this, number, Toast.LENGTH_LONG).show();
+                 phoneNumber = number;
+             } else{
+                 Toast.makeText(this,"please enter a mobile phone number",Toast.LENGTH_SHORT).show();
+             }
+         }
+         phones.close();
+     }
+     cursor.close();
+ }
+// sends the sms
+    public void onClickSend(View v)
+    {
+        //sms body coming from user input
+       // String strSMSBody = "This is a test";
 
-    // This callback is invoked when the Speech Recognizer returns.
-// This is where you process the intent and extract the speech text from the intent.
+        SmsManager sms = SmsManager.getDefault();
+        List<String> messages = sms.divideMessage(spokenText);
+        for (String message : messages) {
+            sms.sendTextMessage(phoneNumber, null, message, PendingIntent.getBroadcast(
+                    this, 0, new Intent(ACTION_SMS_SENT), 0), null);
+        }
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("spokenText",spokenText);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.getString("spokenText","");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
